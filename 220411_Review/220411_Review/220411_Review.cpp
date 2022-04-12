f0b8ba06c7a211e23ba7154253c136626b7e0dc9
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-04-12 09:26:24 +0900

    22 04 12 commit #0
    난수 실습 과제 변경

--- 220411_Review/220411_Review/220411_Review.cpp
+++ 220411_Review/220411_Review/220411_Review.cpp
@@ -157,7 +157,11 @@ int main()
 	
 	// 난수 생성
 	srand(time(NULL));
-	numRandom1 = (rand() * rand() - rand() % inputMax1) % 5 + 3;
+	numRandom1 = rand();
+	srand(time(NULL));
+	numRandom1 += rand();
+	numRandom1 %= 5;
+	numRandom1 += 3;
 	
 	// 출력
 	cout << numRandom1 << endl;
@@ -176,8 +180,12 @@ int main()
 
 	// 난수 생성
 	srand(time(NULL));
-	numRandom2 = (rand() * rand() - rand() % inputMax2) % (inputMax2 - inputMin2 + 1) + inputMin2;
-	
+	numRandom2 = rand();
+	srand(time(NULL));
+	numRandom2 += rand();
+	numRandom2 %= inputMax2 - inputMin2 + 1;
+	numRandom2 += inputMin2;
+
 	// 출력
 	cout << numRandom2 << endl;
 
@@ -190,8 +198,12 @@ int main()
 
 	// 난수 생성
 	srand(time(NULL));
-	numRandom3 = (rand() * rand() - rand() % numMax) % 9 + 1;
-	
+	numRandom3 = rand();
+	srand(time(NULL));
+	numRandom3 += rand();
+	numRandom3 %= numMax - numMin + 1;
+	numRandom3 += numMin;
+
 	// 문제 제시
 	cout << "1부터 9까지의 수 중 어느 것일지 맞추시오." << endl;
 	cin >> playerAnswer;
