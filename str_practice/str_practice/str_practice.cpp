ca58d2ef51a6fdaedc02302d7f4a677249a382a8
strange-tiger <jinolee9525@gmail.com>
2022-05-02 08:37:33 +0900

    22 05 02 commit #1
    Feat : rest changes

---
+++ str_practice/str_practice/str_practice.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, w, h;
+	scanf("%d %d %d %d", &x, &y, &w, &h);
+
+	int xEdgeDistance = 0;
+	int yEdgeDistance = 0;
+	int minEdgeDistance = 0;
+
+	xEdgeDistance = (w - x) < x ? w - x : x;
+	yEdgeDistance = (h - y) < y ? h - y : y;
+
+	minEdgeDistance = xEdgeDistance < yEdgeDistance ? xEdgeDistance : yEdgeDistance;
+
+	printf("%d", minEdgeDistance);
+
+	return 0;
+}
